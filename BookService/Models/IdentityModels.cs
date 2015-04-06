785d6f5dea647f86f401f49b13c3d414ccdcf351
keonnaamoni <keonnaevans@gmail.com>
2015-04-06 11:31:34 -0400

    Added BookService Controller

--- BookService/Models/IdentityModels.cs
+++ BookService/Models/IdentityModels.cs
@@ -31,5 +31,7 @@ namespace BookService.Models
         }
 
         public System.Data.Entity.DbSet<BookService.Models.Author> Authors { get; set; }
+
+        public System.Data.Entity.DbSet<BookService.Models.Book> Books { get; set; }
     }
 }
